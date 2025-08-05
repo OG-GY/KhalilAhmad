b2ebd006623c3a10b0c384f4c2c981c7fa740e5b
Khalil Ahmad <iamkhalil2005@gmail.com>
2025-08-05 23:05:55 +0500

    fix: image location changed

--- khalilahmad/src/components/Hero.jsx
+++ khalilahmad/src/components/Hero.jsx
@@ -10,7 +10,7 @@ export default function Hero() {
       <div className="lg:hidden w-full flex justify-center mb-8 mt-8">
         <div className="relative">
           <Image
-            src="/Images/khalilhero.png"
+            src="/khalilhero.png"
             width={300}
             height={400}
             alt="Khalil Ahmad"
